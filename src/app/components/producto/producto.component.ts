e3ceb4e931d0fcd8576bc4ccf58aecb86d0239a0
darwin <darwinj94@hotmail.com>
2017-08-20 17:12:34 -0500

    pagina de producto y ruta lista

--- src/app/components/producto/producto.component.ts
+++ src/app/components/producto/producto.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import{ActivatedRoute} from '@angular/router';
 @Component({
   selector: 'app-producto',
   templateUrl: './producto.component.html',
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductoComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route:ActivatedRoute) {
+    route.params.subscribe(parametros=>{
+      console.log(parametros['id']);
+    });
+ }
 
   ngOnInit() {
   }
