0ab35e283de17d1d20687465a18aa716ce1acaf9
darwin <darwinj94@hotmail.com>
2017-08-20 15:29:17 -0500

    servicio listo firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,13 +4,30 @@ import {Http} from "@angular/http";
 export class InformacionService {
   info:any={};
   cargada:boolean=false;
+  cargada_sobre_nosotros:boolean=false;
+  equipo:any[]=[];
+
   constructor(public http:Http) {
-    this.http.get("assets/data/info.pagina.json")
-              .subscribe(data=>{
-                console.log(data.json());
-                this.info=data.json();
-                this.info=data.json();
-              })
+    this.carga_info();
+    this.carga_sobre_nosotros();
+   }
+
+   public carga_info(){
+     this.http.get("assets/data/info.pagina.json")
+               .subscribe(data=>{
+                 this.info=data.json();
+                 this.cargada=true;
+               })
+   }
+
+   public carga_sobre_nosotros(){
+     this.http.get("https://gag-86cba.firebaseio.com/equipo.json")
+               .subscribe(data=>{
+                 this.cargada_sobre_nosotros=true;
+                 this.equipo=data.json();
+                 console.log(data);
+               })
+
    }
 
 }
